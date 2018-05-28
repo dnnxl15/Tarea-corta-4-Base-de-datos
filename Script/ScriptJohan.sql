b652c86ec3a499f70cc46cd8e9a9c88b9e40923b
jtstarfox21 <jtstarfox21@users.noreply.github.com>
2018-05-27 22:41:56 -0600

    Fixes and update

--- Script/ScriptJohan.sql
+++ Script/ScriptJohan.sql
@@ -3,16 +3,30 @@
  * Description: Creates the table Combo with its respective attributes
  * Author: Johan Torres Creed
  * Created: 22/05/18
- * Last modification: 22/05/18
+ * Last modification: 27/05/18
 */
 
 CREATE TABLE Combo (
 	idCombo int IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	comboCode int,
-	idRestaurant int FOREIGN KEY REFERENCES dbo.restaurant(idRestaurant),
 	productList VARCHAR(255),
-	comboPrice real,
-	isLimitedTime boolean
+	comboPrice money,
+	isLimitedTime bit
+);
+
+/**
+ * DDL Command
+ * Description: Creates the table ComboByRestaurant with its respective attributes
+ * Author: Johan Torres Creed
+ * Created: 27/05/18
+ * Last modification: 27/05/18
+*/
+
+CREATE TABLE ComboByRestaurant (
+	idComboByRestaurant int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	idRestaurant int FOREIGN KEY REFERENCES dbo.Restaurant(idRestaurant),
+	restaurant VARCHAR(255),
+	comboList VARCHAR(255)
 );
 
 /**
@@ -20,7 +34,7 @@ CREATE TABLE Combo (
  * Description: Creates the table Bill with its respective attributes
  * Author: Johan Torres Creed
  * Created: 22/05/18
- * Last modification: 22/05/18
+ * Last modification: 27/05/18
 */
 
 CREATE TABLE Bill (
@@ -30,7 +44,7 @@ CREATE TABLE Bill (
 	idSeller int FOREIGN KEY REFERENCES dbo.Seller(idSeller),
 	restaurant int FOREIGN KEY REFERENCES dbo.restaurant(idRestaurant),
 	billDetails VARCHAR(255),
-	totalToPay real
+	totalToPay money
 );
 
 /**
@@ -38,15 +52,13 @@ CREATE TABLE Bill (
  * Description: Procedure to insert a new combo into Combo table
  * Author: Johan Torres Creed
  * Created: 23/05/18
- * Last modification: 23/05/18
+ * Last modification: 27/05/18
 */
 
-CREATE PROCEDURE insertCombo(@pCode int, @pRestaurant VARCHAR(100), @pProductList VARCHAR(100), @pComboPrice int, @pLimitedTime boolean) AS
+CREATE PROCEDURE insertCombo(@pCode int, @pProductList VARCHAR(100), @pComboPrice money, @pLimitedTime bit) AS
 BEGIN
-DECLARE @pIdRestaurant int
-SELECT @pIdRestaurant = [dbo].[getRestaurantID](@pRestaurant)
-INSERT INTO dbo.Combo(comboCode, idRestaurant, productList, comboPrice, isLimitedTime)
-VALUES(@pCode, @pIdRestaurant, @pProductList, @pComboPrice, @pLimitedTime);
+INSERT INTO dbo.Combo(comboCode, productList, comboPrice, isLimitedTime)
+VALUES(@pCode, @pProductList, @pComboPrice, @pLimitedTime);
 END;
 
 /**
@@ -57,9 +69,41 @@ END;
  * Last modification: 23/05/18
 */
 
-CREATE PROCEDURE getCombo AS
+CREATE PROCEDURE getAllCombo AS
 BEGIN
 SELECT * FROM Combo;
+END;
+
+/**
+ * DDL Command
+ * Description: Function to get combo code from a row in Combo table
+ * Author: Johan Torres Creed
+ * Created: 27/05/18
+ * Last modification: 27/05/18
+*/
+
+CREATE FUNCTION getComboCode(@pId AS INT)
+RETURNS INT
+AS BEGIN
+	DECLARE @code INT
+	SET @code = (SELECT Combo.comboCode FROM Combo WHERE Combo.idCombo = @pId)
+	RETURN @code
+END;
+
+/**
+ * DDL Command
+ * Description: Function to get combo ID from a row in Combo table
+ * Author: Johan Torres Creed
+ * Created: 27/05/18
+ * Last modification: 27/05/18
+*/
+
+CREATE FUNCTION getComboId(@pCode AS INT)
+RETURNS INT
+AS BEGIN
+	DECLARE @comboId INT
+	SET @comboId = (SELECT Combo.idCombo FROM Combo WHERE Combo.comboCode = @pCode)
+	RETURN @comboId
 END;
 
 /**
@@ -67,20 +111,17 @@ END;
  * Description: Procedure to update an existing combo in Combo table
  * Author: Johan Torres Creed
  * Created: 23/05/18
- * Last modification: 23/05/18
+ * Last modification: 27/05/18
 */
 
-CREATE PROCEDURE updateCombo(@pId int, @pCode int, @pRestaurant VARCHAR(100), @pProductList VARCHAR(100), @pComboPrice int, @pLimitedTime boolean) AS
+CREATE PROCEDURE updateCombo(@pOldCode int, @pNewCode int, @pProductList VARCHAR(100), @pComboPrice int, @pLimitedTime bit) AS
 BEGIN
-DECLARE @pIdRestaurant int
-SELECT @pIdRestaurant = [dbo].[getRestaurantID](@pRestaurant)
 UPDATE dbo.Combo
-SET comboCode = @pCode,
- idRestaurant = @pIdRestaurant,
+SET comboCode = @pNewCode,
  productList = @pProductList,
  comboPrice = @pComboPrice,
  isLimitedTime = @pLimitedTime
-WHERE Combo.idCombo = @pId
+WHERE Combo.comboCode = @pOldCode
 END;
 
 /**
@@ -102,19 +143,13 @@ END;
  * Description: Procedure to insert a new bill into Bill table
  * Author: Johan Torres Creed
  * Created: 23/05/18
- * Last modification: 23/05/18
+ * Last modification: 27/05/18
 */
 
-CREATE PROCEDURE insertBill(@pCode int, @pClient VARCHAR(100), @pSeller VARCHAR(100), @pRestaurant VARCHAR(100), @pBillDetails VARCHAR(100), @pTotal real) AS
-DECLARE @pIdClient int
-DECLARE @pIdSeller int
-DECLARE @pIdRestaurant int
-SELECT @pIdClient = [dbo].[getClientID](@pClient),
-@pIdSeller = [dbo].[getSellerID](@pSeller),
-@pIdRestaurant = [dbo].[getRestaurantID](@pRestaurant)
+CREATE PROCEDURE insertBill(@pCode int, @pClientIdent int, @pSellerIdent int, @pRestaurant VARCHAR(20), @pBillDetails VARCHAR(100), @pTotal money) AS
 BEGIN
-INSERT INTO dbo.Bill(billCode, idClient, idSeller, idRestaurant, billDetails, totalToPay)
-VALUES (@pCode, @pIdClient, @pIdSeller, @pIdRestaurant, @pBillDetails, @pTotal);
+INSERT INTO dbo.Bill(billCode, idClient, idSeller, restaurant, billDetails, totalToPay)
+VALUES (@pCode, dbo.getClientId(@pClientIdent), dbo.getSellerId(@pSellerIdent), dbo.getRestaurantID(@pRestaurant), @pBillDetails, @pTotal);
 END;
 
 /**
@@ -125,7 +160,7 @@ END;
  * Last modification: 23/05/18
 */
 
-CREATE PROCEDURE getBill AS
+CREATE PROCEDURE getAllBill AS
 BEGIN
 SELECT * FROM Bill;
 END;
@@ -135,25 +170,19 @@ END;
  * Description: Procedure to update an existing bill in Bill table
  * Author: Johan Torres Creed
  * Created: 23/05/18
- * Last modification: 23/05/18
+ * Last modification: 27/05/18
 */
 
-CREATE PROCEDURE updateBill(@pId int, @pCode int, @pClient VARCHAR(100), @pSeller VARCHAR(100), @pRestaurant VARCHAR(100), @pBillDetails VARCHAR(100), @pTotal real) AS
+CREATE PROCEDURE updateBill(@pOldCode int, @pNewCode int, @pClientIdent int, @pSellerIdent int, @pRestaurant VARCHAR(20), @pBillDetails VARCHAR(100), @pTotal money) AS
 BEGIN
-DECLARE @pIdClient int
-DECLARE @pIdSeller int
-DECLARE @pIdRestaurant int
-SELECT @pIdClient = [dbo].[getClientID](@pClient),
-@pIdSeller = [dbo].[getSellerID](@pSeller),
-@pIdRestaurant = [dbo].[getRestaurantID](@pRestaurant)
 UPDATE dbo.Bill
-SET billCode = @pCode,
- idClient = @pIdClient,
- idSeller = @pIdSeller,
- idRestaurant = @pIdRestaurant,
+SET billCode = @pNewCode,
+ idClient = dbo.getClientId(@pClientIdent),
+ idSeller = dbo.getSellerId(@pSellerIdent),
+ restaurant = dbo.getRestaurantID(@pRestaurant),
  billDetails = @pBillDetails,
  totalToPay = @pTotal
-WHERE Bill.idBill = @pId
+WHERE Bill.billCode = @pOldCode
 END;
 
 /**
